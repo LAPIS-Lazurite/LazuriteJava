54a58a79277405a3b8540199f05462c7917add0c
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2016-03-17 11:42:35 +0900

    change folder name from lib to src

--- lib/com/lapis_semi/lazurite/io/RawEventListener.java
+++
@@ -1,7 +0,0 @@
-package com.lapis_semi.lazurite.io;
-
-import java.util.*;
-
-public interface RawEventListener extends EventListener {
-	public void RawDataAvailable(RawEventObject ev);
-}
--- lib/com/lapis_semi/lazurite/io/SubGHzEventListener.java
+++
@@ -1,7 +0,0 @@
-package com.lapis_semi.lazurite.io;
-
-import java.util.*;
-
-public interface SubGHzEventListener extends EventListener {
-	public void SubGHzEvent(SubGHzEventObject ev);
-}
